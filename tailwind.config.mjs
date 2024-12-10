619a6dd56512cb37ee1a11bc1154ca112e62e744
Evan X <64918172+evanetizen@users.noreply.github.com>
2024-12-10 17:27:46 -0500

    refactor to tailwind

---
+++ tailwind.config.mjs
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        pixel: [
+          "Silkscreen",
+          "ui-monospace",
+          "SFMono-Regular",
+          "Menlo",
+          "Monaco",
+          "Consolas",
+          "Liberation Mono",
+          "Courier New",
+        ],
+      },
+    },
+  },
+  plugins: [require("@tailwindcss/typography")],
+  darkMode: "selector",
+};
