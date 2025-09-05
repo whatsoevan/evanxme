b286f185a5db4889be99085f7d13daa1ea88c535
Evan X <64918172+evanetizen@users.noreply.github.com>
2025-09-04 21:58:15 -0400

    congfig updates

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -4,24 +4,26 @@ export default {
   theme: {
     extend: {
       fontFamily: {
-        pixel: [
-          "Silkscreen",
-          "ui-monospace",
-          "SFMono-Regular",
-          "Menlo",
-          "Monaco",
-          "Consolas",
-          "Liberation Mono",
-          "Courier New",
+        sans: [
+          "Inter",
+          "ui-sans-serif",
+          "system-ui",
+          "-apple-system",
+          "BlinkMacSystemFont",
+          "Segoe UI",
+          "Roboto",
+          "Helvetica Neue",
+          "Arial",
+          "Noto Sans",
+          "sans-serif",
         ],
-        sentient: [
-          "Sentient",
+        serif: [
+          "Besley",
           "ui-serif",
+          "Georgia",
+          "Times New Roman",
+          "Times",
           "serif",
-          "Apple Color Emoji",
-          "Segoe UI Emoji",
-          "Segoe UI Symbol",
-          "Noto Color Emoji",
         ],
       },
     },
